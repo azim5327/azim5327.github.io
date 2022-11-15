357c1dd6668a4c42ad5098be0f1bd43bd141bb06
azim5327 <114977136+azim5327@users.noreply.github.com>
2022-11-15 12:40:16 +0800

    ChangeBackgroundColor.file

--- app.js
+++ app.js
@@ -1,19 +1,12 @@
-const hexNumbers = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
-const hexBtn = document.querySelector('.hexBtn');
+const colorBtn = document.querySelector('.colorBtn');
 const bodyBcg = document.querySelector('body');
-const hex = document.querySelector('.hex');
+const colors = ['yellow','red','green','3b35998'];
 
-hexBtn.addEventListener('click',getHex);
+colorBtn.addEventListener('click',changeColor);
 
-function getHex(){
-    let hexCol = '#';
-
-    for(let i = 0; i<6;i++){
-        let random = Math.floor(Math.random()*hexNumbers.length);
-        //console.log(random);
-        hexCol +=hexNumbers[random];
-    }
-    bodyBcg.style.backgroundColor = hexCol;
-    hex.innerHTML = hexCol;
-
+function changeColor(){
+    //bodyBcg.style.backgroundColor = colors[2];
+    let random = Math.floor(Math.random()*colors.length)
+    bodyBcg.style.backgroundColor = colors[random];
 }
+    
